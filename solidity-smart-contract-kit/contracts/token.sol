30b6fed9699e1a31b99689b9292ea17feb431e8b
Kyle Galloway <123207266+Kyle-Galloway01@users.noreply.github.com>
2024-03-20 12:48:07 -0500

    Update token.sol

--- solidity-smart-contract-kit/contracts/token.sol
+++ solidity-smart-contract-kit/contracts/token.sol
@@ -1,51 +1,47 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.11;
 
 contract Token {
     string public name;
     string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
+    uint8 public immutable decimals;
+    uint256 public immutable totalSupply;
     mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        totalSupply = _initialSupply * (10 ** uint256(decimals));
-        balanceOf[msg.sender] = totalSupply;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(_to != address(0), "Invalid address");
+    function transfer(address _to, uint256 _value) external returns (bool success) {
         require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-
-        _transfer(msg.sender, _to, _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(_from, _to, _value);
-    }
-
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
+    function approve(address _spender, uint256 _value) external returns (bool success) {
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_from != address(0) && _to != address(0), "Invalid addresses");
-        require(balanceOf[_from] >= _value && allowance[_from][msg.sender] >= _value, "Insufficient allowance or balance");
-
-        _transfer(_from, _to, _value);
-        allowance[_from][msg.sender] -= _value;
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success) {
+        require(balanceOf[_from] >= _value, "Insufficient balance");
+        require(_value <= allowance(_from, msg.sender), "Allowance exceeded");
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
         return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return 0; // Implement allowance logic here
     }
 }
 
